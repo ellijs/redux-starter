1ef692166e75b63c0580f9855d5715451474a378
ellijs <ellijs@hotmail.com>
2021-12-24 01:23:54 -0500

    redux

---
+++ redux-starter/src/actions.js
@@ -0,0 +1,19 @@
+import * as actions from './actionTypes'
+
+export const bugAdded = description => ({
+    type: actions.BUG_ADDED,
+    payload: {
+        description: "Bug1"
+    }
+})
+
+// export function bugAdded(description) {{
+//     return {
+//         type : actions.BUG_ADDED,
+//         payload : {
+//             description: "Bug1"
+//         }
+//     }
+// }}
+
+
--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 
 import store from './store'
 import * as actions from './actionTypes'
+import { bugAdded } from "./actions"
 
 // state = reducer(state, action)
 // notify the subscribers
@@ -14,12 +15,15 @@ const unsubscribe = store.subscribe(()=> {
   console.log("Store changed!", store.getState())
 })
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload:  {
-    description : "Bug1"
-  }
-})
+// store.dispatch({
+//   type: actions.BUG_ADDED,
+//   payload:  {
+//     description : "Bug1"
+//   }
+// })   ==> moved to actions
+// changed to below
+
+store.dispatch(bugAdded("Bug 1"))
 
 unsubscribe()
 
