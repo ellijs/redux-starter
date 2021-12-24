0dfe35c191d8713310713203b207be2970709e02
ellijs <ellijs@hotmail.com>
2021-12-24 01:24:36 -0500

    arrow function

--- redux-starter/src/actions.js
+++ redux-starter/src/actions.js
@@ -7,13 +7,13 @@ export const bugAdded = description => ({
     }
 })
 
-export function bugAdded(description) {{
-    return {
-        type : actions.BUG_ADDED,
-        payload : {
-            description: "Bug1"
-        }
-    }
-}}
+// export function bugAdded(description) {{
+//     return {
+//         type : actions.BUG_ADDED,
+//         payload : {
+//             description: "Bug1"
+//         }
+//     }
+// }}
 
 
